d01b12f46ab6b8c6843171c23a36c35ac10164a2
AdamLebo <alieberman10@aol.com>
2023-02-26 22:30:07 -0500

    created main component

---
+++ portfolio/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { HiArrowNarrowRight } from 'react-icons/hi'
+
+const main = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#0E0B16]'>
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <h1 className='text-3xl sm:text-6xl font-bold text-[#A239CA]'>Adam Lieberman</h1>
+        </div>
+    </div>
+  )
+}
+
+export default main
